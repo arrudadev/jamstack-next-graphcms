d7e72541f960bea6c06a1f4d1515a5cce0224945
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-02-20 21:33:41 -0300

    feat: load Home Page data in server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
-import { usePageQuery } from '../generated/graphql';
+import { GetServerSideProps } from 'next';
+
+import { usePageQuery, PageDocument } from '../generated/graphql';
+import { client, ssrCache } from '../lib/urql';
 
 export default function Home() {
   const [{ data }] = usePageQuery({
@@ -9,3 +12,13 @@ export default function Home() {
 
   return <h1>{data?.page?.title}</h1>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: 'home' }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
